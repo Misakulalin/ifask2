527a8d5be43e302582f2f2dd7b32803ee7d468c1
Misakulalin <zhengpeng152152@gmail.com>
2025-08-12 20:24:14 +0800

    添加依赖
    配置完了grpc依赖，为之后的邮箱验证服务提供了基石。

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
 #include "ifask2.h"
 #include<qfile.h>
 #include <QApplication>
+#include<global.h>
 
 int main(int argc, char *argv[])
 {
-    QFile qss(":/style/stylesheet.qss");
     QApplication a(argc, argv);
+    
+    // 加载QSS样式
+    QFile qss(":/style/stylesheet.qss");
+    if (qss.open(QFile::ReadOnly)) {
+        QString styleSheet = QLatin1String(qss.readAll());
+        qApp->setStyleSheet(styleSheet);
+        qss.close();
+    }
+    // 获取当前应用程序的路径
+    QString app_path = QCoreApplication::applicationDirPath();
+    // 拼接文件名
+    qDebug()<<"path is"<<app_path;
+    QString fileName = "config.ini";
+    QString config_path = QDir::toNativeSeparators(app_path +QDir::separator() + fileName);
+    qDebug() << "Config file path:" << config_path;
+    QSettings settings(config_path, QSettings::IniFormat);
+    QString gate_host = settings.value("GateServer/host").toString();
+    qDebug()<<gate_host<<"is";
+    QString gate_port = settings.value("GateServer/port").toString();
+    qDebug()<<gate_port<<"gate_port";
+    gate_url_prefix = "http://"+gate_host+":"+gate_port;
     ifask2 w;
     w.show();
     return a.exec();
